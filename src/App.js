770332012b52bffb0c9c83c0f5abd53a8bb8abd4
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-08 16:29:24 +0100

    hero, nav & moviegrid imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import MovieGrid from './component/MovieGrid';
+import requests from './config/requests';
+import Nav from './component/Nav';
+import Hero from './component/hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Homepage </h1>
-      </header>
+      <Nav />
+      <Hero
+        fetchUrl={requests.popular} />
+      <MovieGrid
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.netflix} />
+      <MovieGrid
+        title="TRENDING NOW"
+        fetchUrl={requests.trending} />
+      <MovieGrid
+        title="TOP RATED"
+        fetchUrl={requests.topRated} />
+      <MovieGrid
+        title="POPULAR MOVIES"
+        fetchUrl={requests.popular} />
     </div>
   );
 }
