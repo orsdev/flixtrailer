93644046bcc987f90e1fc7354329393a18c2ed30
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-09 10:18:21 +0100

    Spinner & higher order component added to MovieGrid.js

--- src/component/MovieGrid.js
+++ src/component/MovieGrid.js
@@ -3,24 +3,29 @@ import axios from "../axios/axios";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
 import { idIsValid } from "./utils/idIsValid";
+import Spinner from "./Spinner";
 import "../assets/css/moviegrid.css";
+import withErrorHandler from "./withErrorHandler";
+
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 const MovieGrid = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+  const [populateMovie, setpopulateMovie] = useState(false);
 
   useEffect(() => {
     async function fetchMovies() {
       const request = await axios.get(fetchUrl);
-      if (request.status === 200) {
+      if (request && request.status === 200) {
         setMovies(idIsValid(request.data.results));
+        setpopulateMovie(true);
       }
     }
 
     fetchMovies();
-  }, [fetchUrl]);
+  }, [fetchUrl, populateMovie]);
 
   const handleClick = (movie) => {
     if (trailerUrl) {
@@ -49,28 +54,30 @@ const MovieGrid = ({ title, fetchUrl }) => {
     <div className="movies__wrapper px-5">
       <h2 className="text-light movies__title">{title}</h2>
       <div className="movies">
-        <div className="movies__item">
-          {
-            movies && movies.map((movie) => {
-              return (
-                <section key={movie.id}>
-                  <div className="movies__item-poster">
-                    <img src={`${baseUrl}${movie.backdrop_path}`}
-                      alt={movie.name} />
-                  </div>
-                  <div className="movies__item-about">
-                    <h3 className="px-2 py-2">{movie?.name || movie?.title}</h3>
-                    <p >{movie.release_date}</p>
-                  </div>
-                  <button
-                    className="btn px-2 py-2"
-                    onClick={() => { handleClick(movie) }}
-                    data-id={movie.id}>Watch The Trailer</button>
-                </section>
-              )
-            })
-          }
-        </div>
+        {movies.length > 0 ?
+          <div className="movies__item">
+            {
+              movies.length && movies.map((movie) => {
+                return (
+                  <section key={movie.id}>
+                    <div className="movies__item-poster">
+                      <img src={`${baseUrl}${movie.backdrop_path}`}
+                        alt={movie.name} />
+                    </div>
+                    <div className="movies__item-about">
+                      <h3 className="px-2 py-2">{movie?.name || movie?.title}</h3>
+                      <p >{movie.release_date}</p>
+                    </div>
+                    <button
+                      className="btn px-2 py-2"
+                      onClick={() => { handleClick(movie) }}
+                      data-id={movie.id}>Watch The Trailer</button>
+                  </section>
+                )
+              })
+            }
+          </div>
+          : <Spinner />}
         <div className="container-fluid mt-2">
           {trailerUrl &&
             <YouTube
@@ -84,4 +91,4 @@ const MovieGrid = ({ title, fetchUrl }) => {
   );
 };
 
-export default MovieGrid;
+export default withErrorHandler(MovieGrid, axios);
