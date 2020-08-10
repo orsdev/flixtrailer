db1f5fad57681f530ee57c89ccce4fe4b94a333d
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-10 10:45:31 +0100

    hero component refactored

--- src/component/hero.js
+++ src/component/hero.js
@@ -16,7 +16,7 @@ const Hero = () => {
       .then(function (response) {
         const data = idIsValid(response.data.results);
         const random = Math.floor(Math.random() * data.length - 1);
-        if (response && response.status === 200) {
+        if (response && response.status) {
           setMovie([data[random]]);
           setpopulateMovie(true);
         }
@@ -38,37 +38,14 @@ const Hero = () => {
                 {item?.original_title || item?.title || item?.name}
               </h1>
               <img src={`${baseUrl}${item?.backdrop_path}`}
-                alt={item.original_title}
-              />
+                alt={item?.original_title || item?.title || item?.name} />
               <p className="hero__body-about ml-4 my-5">
                 {item?.overview.length && item?.overview.substring(0, 150) + '...'}
               </p>
-              <button
-                className="d-block btn-lg btn btn-danger ml-4 hero__body-btn">
-                Watch Trailer
-          </button>
             </Fragment>
           )
         })
         }
-        <div className="hero__form mx-auto text-center">
-          <h3 className="my-3">
-            Ready to watch? Enter your email to watch trailers.
-          </h3>
-          <form action="" method="post">
-            <div className="form-group">
-              <input
-                className="px-2 form-control"
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email Address"
-                required
-              />
-              <button className="btn btn-dark btn-lg">GET STARTED</button>
-            </div>
-          </form>
-        </div>
       </div>
     </div>
   )
