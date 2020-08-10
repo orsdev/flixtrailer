65e72d5a51807ac943330574a32731f65e29e9fc
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-10 10:46:01 +0100

    Routing and Redux support added to MovieGrid component

--- src/component/MovieGrid.js
+++ src/component/MovieGrid.js
@@ -1,93 +1,102 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "../axios/axios";
-import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
 import { idIsValid } from "./utils/idIsValid";
+import { withRouter } from "react-router-dom";
+import Backdrop from "./Backdrop";
 import Spinner from "./Spinner";
 import "../assets/css/moviegrid.css";
+import { connect } from 'react-redux';
+import * as actions from "../store/actions";
 import withErrorHandler from "./withErrorHandler";
+import LoadMovies from "./LoadMovies";
 
+const MovieGrid = (props) => {
 
-const baseUrl = "https://image.tmdb.org/t/p/original/";
+  const { title, fetchUrl, error } = props;
+  const { trailerUrl, onSetURL, onRemoveURL, history } = props;
 
-const MovieGrid = ({ title, fetchUrl, error }) => {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
+  const [loadTrailer, setLoadTrailer] = useState(false);
 
   useEffect(() => {
     async function fetchMovies() {
       const request = await axios.get(fetchUrl);
-      if (request && request.status === 200) {
-        setMovies(idIsValid(request.data.results));
+      if (request && request.status) {
+        const response = idIsValid(request.data.results);
+        setMovies(response);
       }
     }
 
     fetchMovies();
   }, [fetchUrl]);
 
-  const handleClick = (movie) => {
+  useEffect(() => {
     if (trailerUrl) {
-      setTrailerUrl("");
-    } else {
-      movieTrailer(movie?.name || movie?.title || "")
-        .then((url) => {
-          const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get('v'));
-        })
-        .catch((error) => console.log(error))
+      history.push('/trailer')
     }
-  }
+  }, [trailerUrl]);
 
-  const opts = {
-    height: '300',
-    width: '300',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      origin: 'https://www.youtube.com/',
-      enablejsapi: '1',
-    },
+
+  const handleClick = (movie) => {
+
+    setLoadTrailer(true);
+
+    if (trailerUrl) {
+      onRemoveURL();
+    }
+
+    movieTrailer(movie?.name || movie?.title || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        onSetURL(urlParams.get('v'));
+      })
+      .catch((error) => {
+        console.log('bad things happen');
+        setLoadTrailer(false);
+        onRemoveURL();
+      })
   }
 
   return (
-    <div className="movies__wrapper px-5">
-      <h2 className="text-light movies__title">{title}</h2>
-      <div className="movies">
-        {
-          !error ?
-            <div className="movies__item">
-              {
-                (movies.length) ? movies.map((movie) => {
-                  return (
-                    <section key={movie.id}>
-                      <div className="movies__item-poster">
-                        <img src={`${baseUrl}${movie?.backdrop_path}`}
-                          alt={movie.name} />
-                      </div>
-                      <div className="movies__item-about">
-                        <h3 className="px-2 py-2">{movie?.name || movie?.title}</h3>
-                        <p >{movie?.release_date}</p>
-                      </div>
-                      <button
-                        className="btn px-2 py-2"
-                        onClick={() => { handleClick(movie) }}>
-                        Watch The Trailer</button>
-                    </section>
-                  )
-                })
-                  : <Spinner />
-              }
-            </div> : null}
-        <div className="container-fluid mt-2">
-          {trailerUrl &&
-            <YouTube
-              className="video-youtube"
-              videoId={trailerUrl}
-              opts={opts} />
-          }
+    <Fragment>
+      {!loadTrailer ?
+        <div className="movies__wrapper px-5">
+          <h2 className="text-light movies__title">{title}</h2>
+          <div className="movies">
+            {
+              !error ?
+                <div className="movies__item">
+                  {
+                    (movies.length) ?
+                      <LoadMovies
+                        movies={movies}
+                        onClick={handleClick} />
+                      : <Spinner />
+                  }
+                </div> : null}
+          </div>
         </div>
-      </div>
-    </div>
+        :
+        <Backdrop>
+          <Spinner />
+        </Backdrop>
+      }
+    </Fragment>
   );
 };
 
-export default withErrorHandler(MovieGrid, axios);
+const mapStateToProps = (state) => {
+  return {
+    trailerUrl: state.trailerUrl.url
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSetURL: (url) => dispatch(actions.initTrailerUrl(url)),
+    onRemoveURL: () => dispatch(actions.removeTrailerUrl())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(withRouter(MovieGrid), axios));
