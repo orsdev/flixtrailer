2d53b356cf3fa95d6014e683d73927cf8cd4cf77
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-10 10:46:56 +0100

    Popup component refactored( useless code removed)

--- src/component/Popup.js
+++ src/component/Popup.js
@@ -1,24 +1,23 @@
 import React, { Fragment } from "react";
 import "../assets/css/popup.css";
 
-const Popup = ({ show, onClick }) => {
+const Popup = ({ show, onClick, errorType, errorMessage }) => {
   return (
     <Fragment>
-      {show !== null && <div
-        className="overlay">
+      <div
+        className="popup--overlay">
         <div className="popup">
           <div className="popup--header">
-            <h2>NETWORK ERROR</h2>
+            <h2>{errorType}</h2>
             <button
               onClick={onClick}
               className="popup--close">&times;</button>
           </div>
           <p className="popup--content">
-            Please connect to the internet and refresh your browser.
-		    </p>
+            {errorMessage}
+          </p>
         </div>
       </div>
-      }
     </Fragment>
   )
 };
